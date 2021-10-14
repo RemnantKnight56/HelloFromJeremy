47dbd2f21dbfa4c59f2f5ccbb1453616c3238eb2
emLemmon4 <77080516+emlemmon@users.noreply.github.com>
2021-10-14 14:17:01 -0600

    Added second println

--- src/cole/Main.java
+++ src/cole/Main.java
@@ -3,6 +3,9 @@ package cole;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Jeremy!");
+
+        System.out.println("Hello, my name is Jeremy!");
+        System.out.println("Hi Jeremy, my name is Emily.");
     }
+
 }
